84d17eceea6bd5fa25adae5d249fa31962da4f03
amrods <miguel@alpha.local>
2017-04-22 19:05:26 -0600

    Problem #44

---
+++ 044.jl
@@ -0,0 +1,29 @@
+# https://projecteuler.net/problem=44
+# not complete
+
+const N = 2_000
+pentagonal(n::Int) = div(n*(3n - 1), 2)
+
+const pentset = [pentagonal(i) for i in 1:N]
+
+function ispentagonal(n::Int)
+    return n ∈ pentset
+end
+
+function g()
+    D = pentagonal(N)
+    ca = 0
+    cb = 0
+    for a in pentset, b in pentset
+        if ispentagonal(a + b) && ispentagonal(a - b)
+            if abs(a - b) < D
+                D = abs(a - b)
+                ca = a
+                cb = b
+            end
+        end
+    end
+    return D
+end
+
+g() |> println
